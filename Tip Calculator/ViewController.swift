b3b92c4a2ff2511b7a08fa2a7b6df8399bb4ddf2
Janice Kartika <janice.kartika@harukaedu.com>
2017-02-11 18:23:52 +0700

    Initial commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -9,7 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var segment: UISegmentedControl!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
+    @IBAction func calculate(_ sender: Any) {
+        if let input = billAmount.text {
+            var totalAmount = 0.0
+            if let value = Double(input) {
+                switch segment.selectedSegmentIndex {
+                case 0:
+                    totalAmount = 0.1 * Double(value)
+                    break
+                case 1:
+                    totalAmount = 0.15 * Double(value)
+                    break
+                default:
+                    totalAmount = 0.2 * Double(value)
+                }
+                let tipAmount = value + totalAmount
+                totalAmountLabel.text = "$ " + String(totalAmount)
+                tipAmountLabel.text = "$ " + String(tipAmount)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +45,4 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
